a69fe1e1de4753157c2203c10b5174fb62631c95
Max Gorankov <maksim.gorankov@gmail.com>
2020-03-03 17:25:26 +0500

    Add project files.

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Calculator
+{
+    class Calculation<T>
+    {
+        public T Multiply<T>(T a, T b)
+        {
+            ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                paramB = Expression.Parameter(typeof(T), "b");
+
+            BinaryExpression body = Expression.Multiply(paramA, paramB);
+
+            Func<T, T, T> add = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+            return add(a, b);
+        }
+
+        public T Divide<T>(T a, T b)
+        {
+            ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                paramB = Expression.Parameter(typeof(T), "b");
+
+            BinaryExpression body = Expression.Divide(paramA, paramB);
+
+            Func<T, T, T> add = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+            return add(a, b);
+        }
+
+        public T Subtract<T>(T a, T b)
+        {
+            ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                paramB = Expression.Parameter(typeof(T), "b");
+
+            BinaryExpression body = Expression.Subtract(paramA, paramB);
+
+            Func<T, T, T> add = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+            return add(a, b);
+        }
+
+        public T Sum<T>(T a, T b)
+        {
+            ParameterExpression paramA = Expression.Parameter(typeof(T), "a"),
+                paramB = Expression.Parameter(typeof(T), "b");
+
+            BinaryExpression body = Expression.Add(paramA, paramB);
+
+            Func<T, T, T> add = Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+            return add(a, b);
+        }
+    }
+}
---
+++ CalculatorTests/CalculationTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Tests
+{
+    [TestFixture()]
+    public class CalculationTests
+    {
+        [Test()]
+        public void ATest()
+        {
+            Assert.Fail();
+        }
+    }
+}
