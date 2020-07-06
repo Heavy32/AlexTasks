91d6a024ad685c4798662b993ec08e9e48628f88
Max Gorankov <maksim.gorankov@gmail.com>
2020-07-06 16:36:20 +0500

    Add decorators

---
+++ AlphabetDecorator/IAlphabet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetDecorator
+{
+    public interface IAlphabet
+    {
+        public char GetA();
+        public char GetB();
+        public char GetC();
+        public char GetD();
+        public char GetE();
+        public char GetF();
+        public char GetG();
+        public char GetH();
+        public char GetI();
+        public char GetJ();
+        public char GetK();
+        public char GetL();
+        public char GetM();
+        public char GetN();
+        public char GetO();
+        public char GetP();
+        public char GetQ();
+        public char GetR();
+        public char GetS();
+        public char GetT();
+        public char GetU();
+        public char GetV();
+        public char GetW();
+        public char GetX();
+        public char GetY();
+        public char GetZ();
+    }
+}
---
+++ AlphabetDecorator/StrangeODecorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetDecorator
+{
+    public class StrangeODecorator : AlphabetDecorator
+    {
+        public StrangeODecorator(IAlphabet decorator) : base(decorator)
+        {
+        }
+
+        public override char GetO()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            return '\u00F8';
+        }
+    }
+}
